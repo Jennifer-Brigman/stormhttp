96321917062deea9b3cb515be83e40540413dd8c
Seth Michael Larson <sethmichaellarson@protonmail.com>
2016-07-15 19:22:50 -0500

    Split HTTP cookie tests into one assert per test

--- stormsurge/tests/test_http_cookies.py
+++ stormsurge/tests/test_http_cookies.py
@@ -2,62 +2,91 @@ import unittest
 
 
 class TestHTTPCookies(unittest.TestCase):
-    def test_create_cookie(self):
+    def test_empty_cookie_to_bytes(self):
         from stormsurge._cookies import HTTPCookies
         cookie = HTTPCookies(b'')
         self.assertEqual(cookie.to_bytes(), b'Cookie: ')
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'')
 
+    def test_single_cookie_to_bytes(self):
+        from stormsurge._cookies import HTTPCookies
         cookie = HTTPCookies(b'a=1;')
         self.assertEqual(cookie.to_bytes(), b'Cookie: a=1;')
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'')
 
+    def test_multiple_cookie_to_bytes(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1;')
         cookie[b'a'] = b'2'
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=2;')
 
-    def test_meta_cookie(self):
-        import datetime
+    def test_meta_cookie_http_only(self):
         from stormsurge._cookies import HTTPCookies
         cookie = HTTPCookies(b'a=1;')
         cookie.set_meta(b'a', http_only=True)
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=1; HttpOnly;')
 
+    def test_meta_cookie_secure(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1;')
         cookie.set_meta(b'a', secure=True)
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=1; Secure;')
 
+    def test_meta_cookie_expires(self):
+        from stormsurge._cookies import HTTPCookies
+        import datetime
+        cookie = HTTPCookies(b'a=1;')
         cookie.set_meta(b'a', expires=datetime.datetime.utcfromtimestamp(1))
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=1; Expires=Thu, 01 Jan 1970 00:00:01 UTC;')
 
+    def test_meta_cookie_path(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1;')
         cookie.set_meta(b'a', path=b'/test')
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=1; Path=/test;')
 
+    def test_meta_cookie_domain(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1;')
         cookie.set_meta(b'a', domain=b'.example.com')
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=1; Domain=.example.com;')
 
     def test_expire_cookie(self):
         import datetime
-        from stormsurge._cookies import HTTPCookies, _COOKIE_EXPIRE_FORMAT
+        from stormsurge._cookies import HTTPCookies
         cookie = HTTPCookies(b'a=1;')
         cookie.expire_cookie(b'a')
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=; Expires=Thu, 01 Jan 1970 00:00:00 UTC;')
 
+    def test_set_meta_max_age_only(self):
+        import datetime
+        from stormsurge._cookies import HTTPCookies, _COOKIE_EXPIRE_FORMAT
         cookie = HTTPCookies(b'a=1;')
         cookie.set_meta(b'a', max_age=0)
         now = datetime.datetime.utcnow().strftime(_COOKIE_EXPIRE_FORMAT).encode("latin-1")
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=1; Expires=%b;' % now)
 
+    def test_set_meta_expires_only(self):
+        import datetime
+        from stormsurge._cookies import HTTPCookies, _COOKIE_EXPIRE_FORMAT
         cookie = HTTPCookies(b'a=1;')
         now = datetime.datetime.utcnow()
         cookie.set_meta(b'a', expires=now)
         now = now.strftime(_COOKIE_EXPIRE_FORMAT).encode("latin-1")
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=1; Expires=%b;' % now)
 
+    def test_set_meta_max_age_and_expires_1(self):
+        import datetime
+        from stormsurge._cookies import HTTPCookies, _COOKIE_EXPIRE_FORMAT
         cookie = HTTPCookies(b'a=1;')
         now = datetime.datetime.utcnow()
         cookie.set_meta(b'a', expires=now, max_age=10)
         now = now.strftime(_COOKIE_EXPIRE_FORMAT).encode("latin-1")
         self.assertEqual(cookie.to_bytes(set_cookie=True), b'Set-Cookie: a=1; Expires=%b;' % now)
 
+    def test_set_meta_max_age_and_expires_2(self):
+        import datetime
+        from stormsurge._cookies import HTTPCookies, _COOKIE_EXPIRE_FORMAT
         cookie = HTTPCookies(b'a=1;')
         now = datetime.datetime.now()
         later = now + datetime.timedelta(seconds=10)
@@ -73,6 +102,9 @@ class TestHTTPCookies(unittest.TestCase):
             b'Cookie: b=2; a=1;'
         ])
 
+    def test_cookie_multiple_set(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1; b=2;')
         cookie[b'a'] = b'3'
         cookie[b'b'] = b'4'
         self.assertIn(cookie.to_bytes(set_cookie=True), [
@@ -80,15 +112,28 @@ class TestHTTPCookies(unittest.TestCase):
             b'Set-Cookie: b=4;\r\nSet-Cookie: a=3;'
         ])
 
+    def test_cookie_multiple_iter(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1; b=2;')
         for key in cookie:
             self.assertIn(key, [b'a', b'b'])
-            self.assertIn(cookie[key], [b'3', b'4'])
+            self.assertIn(cookie[key], [b'1', b'2'])
 
+    def test_cookie_multiple_contains(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1;')
         self.assertTrue(b'a' in cookie)
         self.assertFalse(b'c' in cookie)
+
+    def test_cookie_delete(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1;')
         del cookie[b'a']
         self.assertEqual(cookie[b'a'], b'')
 
+    def test_cookie_key_errors(self):
+        from stormsurge._cookies import HTTPCookies
+        cookie = HTTPCookies(b'a=1; b=2;')
         with self.assertRaises(KeyError):
             cookie.set_meta(b'c')
         with self.assertRaises(KeyError):
