1663d62a1878f732e7494eb6ea5e759b875558b8
Seth Michael Larson <sethmichaellarson@protonmail.com>
2016-07-15 20:06:40 -0500

    Add tests for compression

---
+++ stormsurge/tests/test_compression.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestCompression(unittest.TestCase):
+    def test_identity_compression(self):
+        from stormsurge._compress import compress_bytes
+        self.assertEqual(compress_bytes(b'identity', b'abc123'), b'abc123')
+
+    def test_gzip_compression(self):
+        from stormsurge._compress import compress_bytes
+        self.assertEqual(compress_bytes(b'gzip', b'abc123')[5:],
+                         b'\x88\x89W\x02\xffKLJ642\x06\x00\\\xbb\x02\xcf\x06\x00\x00\x00')
+
+    def test_deflate_compression(self):
+        from stormsurge._compress import compress_bytes
+        self.assertEqual(compress_bytes(b'deflate', b'abc123'),
+                         b'KLJ642\x06\x00')
+
+    def test_brotli_compression(self):
+        from stormsurge._compress import compress_bytes
+        self.assertEqual(compress_bytes(b'br', b'abc123'),
+                         b'\x8b\x02\x80abc123\x03')
+
+    def test_invalid_compression(self):
+        from stormsurge._compress import compress_bytes
+        with self.assertRaises(ValueError):
+            compress_bytes(b'abc', b'abc123')
