29061b5770b6a16523addca749cbad132fc8cfa3
Seth Michael Larson <sethmichaellarson@protonmail.com>
2016-07-19 22:37:49 -0500

    Switch back to raw sockets

--- benchmark.py
+++ benchmark.py
@@ -20,11 +20,13 @@ if __name__ == "__main__":
     try:
         import uvloop
         loop = uvloop.new_event_loop()
+        print("Running with uvloop event loop.")
     except ImportError:
         loop = asyncio.new_event_loop()
+        print("Running with asyncio event loop.")
     asyncio.set_event_loop(None)  # Make sure that no structures are relying on asyncio.get_event_loop().
 
     loop.set_default_executor(concurrent.futures.ThreadPoolExecutor())
     app = stormhttp.web.Application(loop)
-    app.router.add_endpoint(b'/', {b'GET'}, stormhttp.router.FileEndPoint(__file__))
+    app.router.add_endpoint(b'/', {b'GET'}, BenchmarkEndPoint(b'a' * (1024 * 100)))
     stormhttp.web.run_app(app)
--- stormhttp/web.py
+++ stormhttp/web.py
@@ -14,40 +14,10 @@ __all__ = [
 
 class Application:
     def __init__(self, loop: asyncio.AbstractEventLoop):
-        self.loop = loop
-        self.router = Router(self.loop)
+        self._loop = loop
+        self.router = Router(self._loop)
 
-    def make_handler(self):
-        """
-        Creates a handler for an asyncio server.
-        :return:
-        """
-        return lambda: RequestHandler(self, self.router)
-
-
-class RequestHandler(asyncio.Protocol):
-    def __init__(self, app: Application, router: Router):
-        self._app = app
-        self._router = router
-        self._loop = app.loop
-        self._socket = None
-        self._queue = asyncio.Queue(loop=self._loop)
-
-    def connection_made(self, transport: asyncio.Transport) -> None:
-        self._socket = transport.get_extra_info("socket")
-        asyncio.ensure_future(self.handle_client(self._socket), loop=self._loop)
-
-    def data_received(self, data: bytes):
-        self._queue.put_nowait(data)
-
-    def connection_lost(self, exc):
-        try:
-            if self._socket is not None:
-                self._socket.close()
-        except OSError:
-            pass
-
-    async def handle_client(self, client: socket.socket) -> None:
+    async def listen_to_client(self, client: socket.socket) -> None:
         """
         Function that receives and waits for a client to send requests.
         :param client: Client socket to read request from.
@@ -58,13 +28,18 @@ class RequestHandler(asyncio.Protocol):
             client.setsockopt(socket.IPPROTO_IP, socket.TCP_NODELAY, 1)
         except (OSError, NameError):
             pass
+
         try:
             while True:
                 request = HTTPRequest()
                 try:
                     request_parser = httptools.HttpRequestParser(request)
                     while not request.is_complete():
-                        data = await self._queue.get()
+                        try:
+                            data = await self._loop.sock_recv(client, 102400)
+                        except OSError:
+                            client.close()
+                            return
                         if len(data) > 0:
                             request_parser.feed_data(data)
                         else:
@@ -78,12 +53,12 @@ class RequestHandler(asyncio.Protocol):
                 except httptools.HttpParserError:
                     client.close()
                     return
-                await self._router.process_request(client, request)
+                await self.router.process_request(client, request)
         except OSError:
             return
 
 
-def run_app(app: Application, host: str="0.0.0.0", port: int=8080, ssl_context: typing.Optional[ssl.SSLContext]=None) -> None:
+def run_app(app: Application, host: str="0.0.0.0", port: int=5000, ssl_context: typing.Optional[ssl.SSLContext]=None) -> None:
     """
     Runs an Application object as an asyncio server.
     :param app: Application to run.
@@ -92,23 +67,26 @@ def run_app(app: Application, host: str="0.0.0.0", port: int=8080, ssl_context: 
     :param ssl_context: Optional SSLContext object.
     :return: None
     """
+    async def _connect_loop(_loop: asyncio.AbstractEventLoop) -> None:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setblocking(False)
+        try:  # Try to reuse the address, not available on all systems.
+            server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        except (NameError, OSError):
+            pass
+
+        server.bind((host, port))
+        server.listen(512)
+        while True:
+            client, _ = await _loop.sock_accept(server)
+            _loop.create_task(app.listen_to_client(client))
+
     scheme = "http"
     if ssl_context is not None:
         scheme = "https"
-
     print("======== Running on {}://{}:{}/ ========".format(scheme, host, port))
     print("(Press CTRL+C to quit)")
 
-    loop = app.loop
-    server = loop.run_until_complete(loop.create_server(
-        app.make_handler(), host, port,
-        ssl=ssl_context, backlog=512, reuse_address=True
-    ))
-
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        server.close()
-        loop.run_until_complete(server.wait_closed())
+    loop = app._loop
+    loop.run_until_complete(_connect_loop(loop))
+    loop.run_forever()
