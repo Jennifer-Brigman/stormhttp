6f1938072a71747d45dc47deddc614adf5949a44
lars4839 <lars4839@umn.edu>
2016-08-03 22:13:57 -0500

    Upgrade brotlipy to 0.4.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     install_requires=[
         "aioredis==0.2.8",
-        "brotlipy==0.3.0",
+        "brotlipy==0.4.0",
         "cchardet==1.0.0",
         "cffi==1.7.0",
         "cryptography==1.4",
